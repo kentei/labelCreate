7ec6fa8b2b059d92bbe8c6ea7bfb5337a23a7472
Kentei <test@com>
2015-12-27 22:01:47 +0900

    定数定義フィールドなど追加

--- createJSLabel.js
+++ createJSLabel.js
@@ -1,8 +1,27 @@
+/* 定数定義フィールド始まり */
+
+/** 読み込むエクセルファイルの絶対パス */
+var EXCEL_FILE_PATH = "D:\\rabel\\labelCreate\\Table.xlsx";
+/** 改行コード */
+var LINE_CODE = "\r\n";
+
+/* 定数定義フィールド終わり*/
+
+/* 実処理 */
+writeFile("Sheet1");
+
+/* 以降は関数定義 */
+
+/**
+ * ファイル出力処理
+ * @param {String} sheetName シート名
+ */
+function writeFile(sheetName) {
 try {
     var excelApp = WScript.CreateObject("Excel.Application");
     //エクセルブックのパス、及びシート名を指定する。
-    var book = excelApp.Workbooks.Open("C:\\xampp\\htdocs\\damaz_hp\\damaz-hp\\developTools\\labelResourceGen\\Table.xlsx");
-    var sheet = book.WorkSheets("Sheet1");
+    var book = excelApp.Workbooks.Open(EXCEL_FILE_PATH);
+    var sheet = book.WorkSheets(sheetName);
     
     var share_code_start = '';  //共通コード(開始)
     var share_code_end = '';  //共通コード(終了)
@@ -15,15 +34,6 @@ try {
     share_code_start = '__Localizer.strings = {';
     //ラベル生成ソース共通の処理を書き込む3(終わり)
     share_code_end = '}\;';
-    
-    //ラベル表示(呼び出し)ソース共通の処理を書き込む1(php宣言)
-    share_code_forDisp += '<?php\n\n';
-    //ラベル表示(呼び出し)ソース共通の処理を書き込む2(言語選択) ※言語を増やす場合はここを編集する。
-    share_code_forDisp += '$lang = setResourceLang(filter_input(INPUT_SERVER, \'HTTP_ACCEPT_LANGUAGE\'));\n\n'
-            + 'if($lang == \'ja\'){\n    require_once(ROOT_PATH.\'php/ja_label_server.php\');\n'
-            + '}else if($lang == \'en\'){\n    require_once(ROOT_PATH.\'php/en_label_server.php\');\n'
-            + '}else{\n    require_once(ROOT_PATH.\'php/en_label_server.php\');\n}\n'
-            + '$l1 = new Localizer_server($lang);\n\n';
     
     //繰返し用の変数
     var r = 0;
@@ -40,17 +50,14 @@ try {
             r++;
             continue;
         }
-        //var test = "$script_i18n[\'" + sheet.Cells(3 + r, 3) + "\'][\'ja\'] = \'" + sheet.Cells(3 + r, 4) + "\'";
-        ja_code += '\'' + sheet.Cells(3 + r, 3) + '\' : \'' + sheet.Cells(3 + r, 4) + '\',\n';
-        en_code += '\'' + sheet.Cells(3 + r, 3) + '\' : \'' + sheet.Cells(3 + r, 5) + '\',\n';
-       //disp_code += '$' + sheet.Cells(3 + r, 3) + ' = $l1->g(\'' + sheet.Cells(3 + r, 3) + '\');\n';
+        ja_code += '\'' + sheet.Cells(3 + r, 3) + '\' : \'' + sheet.Cells(3 + r, 4) + '\',' + LINE_CODE;
+        en_code += '\'' + sheet.Cells(3 + r, 3) + '\' : \'' + sheet.Cells(3 + r, 5) + '\',' + LINE_CODE;
 
-        //ja_Str += test + ";\n";
         r++;
     }
 } catch (e) {
     //例外発生時はここを通る。
-    WScript.Echo('error');
+    WScript.Echo(e);
 } finally {
     book.Close();
     excelApp.Quit();
@@ -58,13 +65,15 @@ try {
 }
 
 //最終的に読み込んだデータをファイルに書き出す
-saveToFile("output/js/ja.js", share_code_start + ja_code + share_code_end);
-saveToFile("output/js/en.js", share_code_start + en_code + share_code_end);
-saveToFile("../dest/js/lang/locale/ja.js", share_code_start + ja_code + share_code_end);
-saveToFile("../dest/js/lang/locale/en.js", share_code_start + en_code + share_code_end);
-saveToFile("../../js/lang/locale/ja.js", share_code_start + ja_code + share_code_end);
-saveToFile("../../js/lang/locale/en.js", share_code_start + en_code + share_code_end);
+saveToFile("output/test.txt", share_code_start + ja_code + share_code_end);
+//saveToFile("output/js/en.js", share_code_start + en_code + share_code_end);
+//saveToFile("../dest/js/lang/locale/ja.js", share_code_start + ja_code + share_code_end);
+//saveToFile("../dest/js/lang/locale/en.js", share_code_start + en_code + share_code_end);
+//saveToFile("../../js/lang/locale/ja.js", share_code_start + ja_code + share_code_end);
+//saveToFile("../../js/lang/locale/en.js", share_code_start + en_code + share_code_end);
 //saveToFile("output/displayLabelList.php", share_code_forDisp + disp_code);
+
+}
 
 /**
  * UTF-8 BOMなしでファイルを作成する
